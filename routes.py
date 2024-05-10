7efd8a49d6c83fa88d0eba91e7c190bda16e6c38
blaiete <168096150+blaiete@users.noreply.github.com>
2024-05-10 13:27:46 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -15,13 +15,12 @@ Monuments: TypeAlias = list[Monument]
 
 class Routes:
     """
-    no tinc ni idea de com implementar o per què servirà routes però més o menys entenc això.
-    Potser routes hauria de ser una llista de Route, que sí tindria aquests atributs. Segons el que
-    sembla en les funcions export, routes és un graf o una llista de rutes
+    Segons el que sembla en les funcions export, routes és un graf o una llista de rutes.
+    Més o menys hauria de tenir aquests atributs
     """
     total_dist: int  # distància total de la ruta
     start: Point
-    end: Monument  # monument on acaba la ruta
+    end: Monument  # monument on acaba la ruta o potser millor tots els monuments
     path: list[Point]  # llista de nodes per on passem
 
 
@@ -33,7 +32,7 @@ def assign_monuments(graph: nx.Graph, endpoints: Monuments) -> None:
     ...
 
 
-def find_shortest_routes(graph: nx.Graph,) -> Routes:  # no entenc què es routes
+def find_shortest_routes(graph: nx.Graph,) -> Routes:  # no entenc bé què es routes
     """
     Find the shortest routes to each monument on the graph. Netxorkx has a funtion
     to find the mst from a nx.Graph
