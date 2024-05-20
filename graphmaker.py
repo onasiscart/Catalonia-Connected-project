e0ecaff359cc1a10a7da9cb5c284b629a82f9a2e
onasiscart <161739530+onasiscart@users.noreply.github.com>
2024-05-20 17:56:32 +0200

    Add files via upload

--- graphmaker.py
+++ graphmaker.py
@@ -1,72 +1,90 @@
 import networkx as nx
 from typing import TypeAlias
-from segments import Point
-...
+from numpy import ndarray
+import numpy as np
+from dataclasses import dataclass
+from sklearn.cluster import KMeans
+from segments import *
+import matplotlib.pyplot as plt
+from haversine import haversine, Unit
 
 
-Node: TypeAlias = tuple[int, Point]  # [node id, coordinates of the node]
+@dataclass
+class Point:
+    lat: float
+    lon: float
+
+
+# TODO:
+# solucionar el import numpy que no funciona i no sé per què
+# afegir distàncies en afegir les arestes
+# nombre mínim de segments per posar una aresta
+
 Edge: TypeAlias = tuple[int, int]  # edges will be between centroids
 
 
-def make_graph(segments: Segments, clusters: int) -> nx.Graph:
+def modify_for_kmeans(segments: Segments) -> ndarray:  # type:ignore
+    points = np.array(
+        [
+            [point.lat, point.lon]
+            for segment in segments
+            for point in [segment.start, segment.end]
+        ]
+    )
+    return points
+
+
+def distance_between_nodes(coord1: Point, coord2: Point) -> float:
+    """"""
+    point1, point2 = (coord1.lat, coord1.lon), (
+        coord2.lat,
+        coord2.lon,
+    )  # format per les dobles assignacions????
+    return haversine(point1, point2, unit=Unit.KILOMETERS)
+
+
+def add_nodes(graph: nx.Graph, centroid_coords: ndarray) -> None:
+    """Prec: G is empty"""
+    for cluster, coords in enumerate(centroid_coords):
+        # nodes tenen el nombre de centroide com a nom i el Point amb les seves coordenades com a atribut
+        graph.add_node(cluster, coord=Point(float(coords[0]), float(coords[1])))
+
+
+def add_edges(graph: nx.Graph, point_labels: ndarray) -> None:
+    """"""
+    for i in range(0, len(point_labels), 2):
+        p1cluster, p2cluster = point_labels[i], point_labels[i + 1]
+        if p1cluster != p2cluster:
+            graph.add_edge(
+                p1cluster,
+                p2cluster,
+                weight=distance_between_nodes(
+                    graph.nodes[p1cluster]["coord"], graph.nodes[p2cluster]["coord"]
+                ),
+            )
+
+
+def make_graph(segments: Segments, clusters: int) -> nx.Graph:  # type:ignore
     """Make a graph from the segments."""
-    #modifiquem segments perquè pugui ser entrat a kmeans
-    ...
-    #1. clustering
-    cluster = KMeans(n_clusters = clusters, ...).fit(point_data)
-    #2. creem el graf
+    # 1. clustering
+    # modifiquem segments perquè pugui ser entrat a kmeans
+    points = modify_for_kmeans(segments)  # type:ignore
+    kmeans = KMeans(n_clusters=clusters)  # type:ignore
+    kmeans.fit(points)
+    # 2. creem el graf
     graph = nx.Graph()
-    #2.afegim els nodes
-    nodes: list[Node] = list(enumerate(kmeans.cluster_centers_))
-    #3. afehim les arestes!
-
-
-
-
-def _distance_between_points(p1: Point, p2: Point) -> float:
-    point1, point2 = (p1.lat, p1.lon), (
-        p2.lat,
-        p2.lon,
-    )  
-    return haversine(point1, point2)
-
-
-def _add_weights(graph: nx.Graph) -> nx.Graph:
-    """
-    Adds the wieghts (the distances) to the graph.
-    """
-    for u, v in graph.edges():
-        graph[u][v]['weight'] =_distance_between_points(u, v)
+    centroid_coords = kmeans.cluster_centers_
+    point_labels = kmeans.labels_
+    add_nodes(graph, centroid_coords)
+    add_edges(graph, point_labels, centroid_coords)
     return graph
 
 
-def _angle_between_points(p1: Point, p2: Point, p3: Point) -> int:
-    """
-    returns the angle in degrees between three points p1 p2 and p3.
-    """
-    v1 = np.array([p2.lat - p1.lat, p2.lon - p1.lon])
-    v2 = np.array([p2.lat - p3.lat, p2.lon - p3.lon])
-    angle_rad = acos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
-    return np.degrees(angle_rad)
-
-
-def _delete_mid_point(graph: nx.Graph, n1: Point, n2: Point, n3: Point) -> None:
-    """
-    deletes an unnecessary point 'n2' in the graph and adds an edge between 
-    """
-    graph.remove_node(n2)
-    graph.add_edge(n1, n3)
-
-
-def simplify_graph(graph: nx.Graph, epsilon: float = 15) -> nx.Graph:
+def simplify_graph(graph: nx.Graph, epsilon: float) -> nx.Graph:
     """Simplify the graph."""
-    for node in graph:
-        if nx.degree(node) == 2:
-            v, u = nx.neighbors(node)
-            if _angle_between_points(u, node, v) > epsilon:
-                _delete_mid_point(graph, v, node, u)
 
 
 def get_graph(segments: Segments, clusters: int, epsilon: float) -> nx.Graph:
-    simplified = simplify_graph(make_graph(segments, clusters), epsilon)
-    return _add_weights(simplified)
+    G = make_graph(segments, clusters)
+    simplified = simplify_graph(G, epsilon)
+    return simplified
