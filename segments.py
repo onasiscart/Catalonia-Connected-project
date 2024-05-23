81be33321ef7a91c44ad673481a9b0bda2571b24
blaiete <168096150+blaiete@users.noreply.github.com>
2024-05-23 18:44:46 +0200

    Create segments.py

---
+++ segments.py
@@ -0,0 +1,167 @@
+import requests
+import os
+import gpxpy
+import staticmap
+import subprocess
+import platform
+from haversine import haversine
+from dataclasses import dataclass
+from typing import TypeAlias
+from datetime import datetime
+from typing import Optional
+from geographical import Point, Zone
+
+
+@dataclass
+class Segment:
+    start: Point
+    end: Point
+
+
+Segments: TypeAlias = list[Segment]
+
+MAX_TIME = 100  # maximum time difference acceptable between two points red like xx : xx (min, sec)
+MAX_DIST = 0.1  # maximum distance in Km acceptable between two points
+
+
+def _distance_between_segments(start: Point, end: Point) -> float:
+    """
+    Returns the distance between two geographical points 'start' and 'end' in Km
+    """
+    point1, point2 = (start.lat, start.lon), (end.lat, end.lon)
+    return haversine(point1, point2)
+
+
+def _format_date_and_time(time: Optional[datetime],) -> Optional[tuple[int, int]]:  # using optional because time can be None when downloading
+    """
+    Converts the string 'time_string'into a tuple which contains the date and time
+    """
+    if time != None:
+        return (int(time.strftime("%Y%m%d")), int(time.strftime("%H%M%S")))
+    return None
+
+
+def _is_segment_valid(
+    p1: Point, p2: Point, p1_time: datetime, p2_time: datetime
+) -> bool:
+    """
+    Returns a bool based on whether the segment 's' is valid for use or not.
+    """
+    p1_time, p2_time = _format_date_and_time(p1_time), _format_date_and_time(p2_time)
+
+    if p1_time[1] > p2_time[1]:
+        return False
+    elif (p2_time[1] - p1_time[1]) > MAX_TIME:
+        return False
+    elif p1_time[0] != p2_time[0]:
+        return False
+    elif _distance_between_segments(p1, p2) > MAX_DIST:
+        return False
+    return True
+
+
+def _download_segments(zone: Zone, filename: str) -> None:
+    """
+    Download all segments in thezone and save them to the file 'filename'.
+    """
+    p1, p2 = zone.bottom_left, zone.top_right
+    zone_string = "{},{}, {},{}".format(p1.lat, p1.lon, p2.lat, p2.lon)
+
+    page = 0
+    with open(filename, "w") as file:
+        while True:
+            # zone a la url ha de ser un string de zone
+            url = f"https://api.openstreetmap.org/api/0.6/trackpoints?bbox={zone_string}&page={page}"
+            response = requests.get(url)
+            gpx_content = response.content.decode("utf-8")
+            gpx = gpxpy.parse(gpx_content)
+
+            if len(gpx.tracks) == 0:
+                break
+
+            for track in gpx.tracks:
+                for segment in track.segments:
+                    if all(point.time is not None for point in segment.points):
+                        segment.points.sort(key=lambda p: p.time)
+                        for i in range(len(segment.points) - 1):
+                            p1, p2 = segment.points[i], segment.points[i + 1]
+                            point1, point2 = Point(
+                                float(p1.latitude), float(p1.longitude)
+                            ), Point(float(p2.latitude), float(p2.longitude))
+                            if _is_segment_valid(point1, point2, p1.time, p2.time):
+                                file.write(
+                                    f"{p1.latitude}, {p1.longitude} - {p2.latitude}, {p2.longitude}\n"
+                                )
+            page += 1
+
+
+def _load_segments(filename: str) -> Segments:
+    """
+    Loads segments from the file 'filename' and returns a list of segments.
+    """
+    segments: Segments = []
+    with open(filename, "r") as file:
+        for line in file:
+            points_data = line.strip().split(" - ")
+            point1_data, point2_data = points_data[0].split(", "), points_data[1].split(
+                ", "
+            )
+
+            point1 = Point(
+                float(point1_data[0]),
+                float(point1_data[1]),
+            )
+            point2 = Point(
+                float(point2_data[0]),
+                float(point2_data[1]),
+            )
+            segments.append(Segment(point1, point2))
+    return segments
+
+
+def get_segments(zone: Zone, filename: str) -> Segments:
+    """
+    Get all cleaned data of the segments in the box.If filename exists, load segments
+    from the file. Otherwise, download segments in the box and save them to the file.
+    """
+    if not os.path.exists(filename):
+        _download_segments(zone, filename)
+    return _load_segments(filename)
+
+
+def _display_map(filename: str) -> None:
+    """
+    Automatically opens the PNG file 'filename' containing the map of segments
+    """
+    if platform.system() == "Windows":
+        os.startfile(filename)
+    elif platform.system() == "Darwin":  # macOS
+        subprocess.run(["open", filename])
+    else:  # Linux and other Unix-like systems
+        subprocess.run(["xdg-open", filename])
+
+
+def show_segments(segments: Segments, filename: str) -> None:
+    """
+    Show all segments in a PNG file using staticmap.
+    """
+    static_map = staticmap.StaticMap(800, 600)
+    for segment in segments:
+        line = staticmap.Line(
+            [
+                (segment.start.lon, segment.start.lat),
+                (segment.end.lon, segment.end.lat),
+            ],
+            color="black",
+            width=1,
+        )
+        static_map.add_line(line)
+    static_map.render().save(
+        filename, format="png"
+    )  # no serveix de res poasr el fromat aquí, cal indicar-ho al cridar la funció amb
+    _display_map(filename)  # un nom filename.png
+
+
+
+if __name__ == '__main__':
+    pass
