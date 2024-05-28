81bd4e75ee09077c3c049caf2374a705c55f3921
blaiete <168096150+blaiete@users.noreply.github.com>
2024-05-28 18:28:57 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,21 +1,11 @@
 import simplekml
 import networkx as nx
 from geographical import Point
-from typing import TypeAlias
 from dataclasses import dataclass
 from haversine import haversine
-import math
+from monuments import Monuments
 from staticmap import *
 from viewer import display_map
-
-
-@dataclass
-class Monument:
-    name: str
-    location: Point
-
-
-Monuments: TypeAlias = list[Monument]
 
 
 @dataclass
@@ -28,8 +18,9 @@ class Route:
 Routes = list[Route]
 
 
-def distance(coord1: Point, coord2: Point) -> float:
+def euclidean_distance(coord1: Point, coord2: Point) -> float:
     """"""
+    return (coord1.lat - coord2.lat)**2 + (coord1.lon - coord2.lon)**2
 
 
 # def search_for_closest_node(graph: nx.Graph, endpoint: Point) -> int:
@@ -48,17 +39,16 @@ def distance(coord1: Point, coord2: Point) -> float:
 #     return closest_node
 
 
-def search_for_closest_node2(graph: nx.Graph, start: Point) -> int:
+def search_for_closest_node2(graph: nx.Graph, coord: Point) -> int:
     """
-    returns the closest node to the starting point requested by the user. LINEAR TIME!!
+    returns the closest node to the point 'location'.
     """
     min_dist, closest_node = -1, 0
     for node in graph.nodes():
         point = (graph.nodes[node]['coord'].lat, graph.nodes[node]['coord'].lon)
-        distance = haversine((start.lat, start.lon), point)
+        distance = haversine((coord.lat, coord.lon), point)
         if distance < min_dist or min_dist == -1:
             min_dist, closest_node = distance, node
-    print('closest node:', closest_node)
     return closest_node
 
 
@@ -68,9 +58,7 @@ def assign_monuments(graph: nx.Graph, endpoints: Monuments) -> None:
     """
     for monument in endpoints:
         closest_node = search_for_closest_node2(graph, monument.location)
-        print('info del closest:', graph[closest_node])
-        graph[closest_node]["monuments"].append(monument)  # type:ignore
-        print('i assigned', monument,'to', closest_node)
+        graph.nodes[closest_node]["monuments"].append(monument)  # type:ignore
         # aquí dalt no sé per què fallen els tipus
 
 
@@ -81,63 +69,60 @@ def _compute_total_lenght(graph: nx.Graph, path: list[int]) -> float:
     return sum(graph[path[i]][path[i + 1]]['weight'] for i in range(len(path) - 1))
 
 
-def find_shortest_routes(graph: nx.Graph, start: int) -> Routes:  # no entenc què és routes
+def find_shortest_routes(graph: nx.Graph, location: int) -> Routes:  # no entenc què és routes
     """
-    Find the shortest routes from the start point to each monument on the graph. 
+    Find the shortest routes from the location point to each monument on the graph. 
     """
-    targets = [node for node in graph.nodes() if graph.nodes[node]['monuments'] != []]
+    targets = [node for node in graph.nodes() if graph.nodes[node]['monuments']]
     routes = []
-    print('targets:', targets)
-
+    
     for target in targets:
         try:
-            path = nx.dijkstra_path(graph, source = start, target = target, weight = 'weight')
-            start_point: Point = graph.nodes[start]['coord']
+            path = nx.dijkstra_path(graph, source = location, target = target, weight = 'weight')
+            start_point: Point = graph.nodes[location]['coord']
             endpoint: Point = graph.nodes[target]['coord']
             point_path: list[Point] = [graph.nodes[node]['coord'] for node in path]
             routes.append(Route(_compute_total_lenght(graph, path), start_point, endpoint, point_path))
 
         except nx.NetworkXNoPath:
-            print(f"No path between {start} and {target}")
+            print(f"No path between {location} and {target}")
 
     return routes
 
 
-def find_routes(graph: nx.Graph, start: Point, endpoints: Monuments) -> Routes:
+def find_routes(graph: nx.Graph, location: Point, endpoints: Monuments) -> Routes:
     """
     Find the shortest route between the starting point and all the endpoints.
     graph és el graf que rebem de graphmaker, o sigui, el clusteritzat i simplificat
     però sense els monuments assignats.
     """
     assign_monuments(graph, endpoints)
-    return find_shortest_routes(graph, search_for_closest_node2(graph, start))
+    return find_shortest_routes(graph, search_for_closest_node2(graph, location))
 
 
 def export_routes_PNG(routes: Routes, filename: str) -> None:
     """
-    Export the graph to a PNG file using staticmap. Segons la capçalera, enten que
-    routes ha de ser un graf??
+    Export the graph to a PNG file using staticmap. 
     """
-    static_map = StaticMap(width=800, height=600)
-    # add start point marker, since all routes start from the same point
-    start_marker = CircleMarker((routes[0].start.lon, routes[0].start.lat), "blue", 10)
-    static_map.add_marker(start_marker)
+    map = StaticMap(800, 600)
     for route in routes:
-        # Create a Line object to represent the route path
-        line = Line(((point.lon, point.lat) for point in route.path), "red", 3)
-        static_map.add_line(line)
-        # Add end markers
-        end_marker = CircleMarker((route.end.lon, route.end.lat), "green", 10)
-        static_map.add_marker(end_marker)
-
-    static_map.render().save(filename)
+        start_marker = CircleMarker((route.start.lon, route.start.lat), 'green', 8)
+        end_marker = CircleMarker((route.end.lon, route.end.lat), 'red', 8)
+        
+        map.add_marker(start_marker)
+        map.add_marker(end_marker)
+        
+        if route.path:
+            line = Line([(point.lon, point.lat) for point in route.path], 'blue', 2)
+            map.add_line(line)
+    
+    map.render().save(filename)
     display_map(filename)
 
 
 def export_routes_KML(routes: Routes, filename: str) -> None:
     """
-    Export the graph to a KML file. Exportem les rutes a un fitxer KML. Podem fer un fitxer
-    que contingui tant el graf com les rutes?
+    Export the graph to a KML file. 
     """
     kml = simplekml.Kml()
     for route in routes:
@@ -148,7 +133,7 @@ def export_routes_KML(routes: Routes, filename: str) -> None:
             coords=[(point.lon, point.lat) for point in route.path],
         )
 
-        # Add start and end points as placemarks
+        # Add location and end points as placemarks
         kml.newpoint(
             name=f"Start: {route.start}",
             coords=[(route.start.lon, route.start.lat)],
